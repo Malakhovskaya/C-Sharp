2a8b935e7f614da35c884d648d62001e96641f2f
Your Name <trandina.e@gmail.com>
2022-12-09 12:21:24 +0300

    add Example027 project

---
+++ HWork_CSharp_4/Example_027/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Сумма цифр в числе {number} = {result(number)}");
+
+int result(int num)
+{
+    int sum = 0;
+    while(num>0)
+    {
+        sum+=num%10;
+        num/=10;
+    }
+    return sum;
+}
