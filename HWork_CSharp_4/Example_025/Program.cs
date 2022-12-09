809960a8216968d2bd57b21e992d923ffd0abc7a
Your Name <trandina.e@gmail.com>
2022-12-09 11:52:52 +0300

    add Example025 project

---
+++ HWork_CSharp_4/Example_025/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+
+Console.Write("Введите степень: ");
+int B = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{A} в степени {B} = {Pow(A,B)}");
+
+int Pow(int num, int rank)
+{
+    if(B==0) return 1;
+
+    int result = num;
+    for (int i = 2; i <= rank; i++)
+    {
+        result *= num;
+    }
+    return result;
+}
