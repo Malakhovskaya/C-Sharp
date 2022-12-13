229ed38b981b9a0b7c51e6a89df0fde3c62d14dc
Your Name <trandina.e@gmail.com>
2022-12-13 11:20:04 +0300

    add Example034 and Example036 projectes

---
+++ HWork_CSharp_5/Example_034/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+ Напишите программу, которая покажет количество чётных чисел в массиве.
+ [345, 897, 568, 234] -> 2*/
+
+int[] startArray = GetArray(6, 100, 999);//ввели трехзначный положит. массив StartArray через функцию(метод GetArray)
+Console.WriteLine(String.Join(" ", startArray)); 
+Console.WriteLine($"Количество четных элементов в массиве = {GetEvenCount(startArray)}"); //выводим кол-во четных элементов
+
+int[] GetArray(int size, int minValue, int maxValue) //метод зеполнения массива рандомными числами (размер, мин и макс числа)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int GetEvenCount(int[] array) //ввели метод, вычисляющий кол-во четных чисел в массиве
+{
+    int count = 0;
+    foreach (var item in array) //циклом foreach перебираем все числа (var item) в массиве array 
+    {
+        if (item % 2 == 0) count++; // если число четное (делится на 2 без остатка), то счетчик +1
+    }
+    return count;
+}
+
---
+++ HWork_CSharp_5/Example_036/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами.
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0*/
+
+int[] startArray = GetArray(6, 0, 10);//ввели одномерный массив StartArray через функцию(метод GetArray)
+Console.WriteLine(String.Join(" ", startArray)); //выводим на консоль через пробел
+Console.WriteLine($"Сумма элементов на нечетных позициях равна = {GetEvenCount(startArray)}"); //выводим сумму элементов на нечетных позициях
+
+int[] GetArray(int size, int minValue, int maxValue) //метод зеполнения массива рандомными числами (размер, мин и макс числа)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+int GetEvenCount(int[] array) //ввели метод, вычисляющий сумму чисел на нечетных индексах в массиве
+{
+    int sum = 0;
+    
+    for (int i = 0; i < array.Length; i++) //циклом for перебираем все индексы от 0 до длины массива
+    {
+        
+        if (i % 2 == 1) //если индекс нечетный, то число этого индекса суммируется
+        sum = sum + array[i];
+        
+    }
+    return sum;
+}
+
