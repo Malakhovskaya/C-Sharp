b5d77faa7ae4d426aa9355b51cb37e1999870889
Your Name <trandina.e@gmail.com>
2022-12-13 10:29:40 +0300

    add more Example project

---
+++ HWork_CSharp_6/Example_041/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+int[] array = GetArray(5, 1, 99);  // вводим одномерный масив
+Console.WriteLine(String.Join(" ", array)); // слепливанием и вывод массива на консоль
+
+
+int [] GetArray(int size, int minValue, int maxValue) // вводим метод заполнения массива рандомными числами
+{
+   int [] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);
+    }
+    return array;
+}
+
+int sum = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) sum += 1;
+    }
+
+
+Console.WriteLine($"Пользователь ввёл {sum} положительных чисел");
---
+++ Seminar_doubleMass/Example_001/Program.cs
@@ -0,0 +1,3 @@
+﻿startArray = GetArray(6, 0, 999);
+Console.WriteLine(String.Join(" ", startArray));
+Console.WriteLine($"Сумма элементов на нечетных позициях = {GetSumOdd(startArray)}");
