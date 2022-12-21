cb209fc3303eaea26a2c323f46bb5664499b6912
Your Name <trandina.e@gmail.com>
2022-12-21 22:10:33 +0300

    add Example062  project

---
+++ HWork_CSHarp_8/Example_062/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07*/
+
+int n = 4; // ввели переменную, равную row and column так как по заданию массив 4х4 
+int[,] Array = new int[n, n]; // ввели двумерный массив размером n = 4x4
+
+
+int number = 1; // присвоили элементу массива number = 1 (начинаяс 1 как по заданию)
+int i = 0;
+int j = 0;
+
+while (number <= Array.GetLength(0) * Array.GetLength(1)) // пока элемент массива <= произведения длин квадратов должны выполняться условия
+{
+  Array[i, j] = number;
+  number++;
+  if (i <= j + 1 && i + j < Array.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= Array.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > Array.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+PrintArray(Array);
+
+void PrintArray(int[,] GetArray)
+{
+    for (int i = 0; i < GetArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < GetArray.GetLength(1); j++)
+        {
+            Console.Write(GetArray[i, j] + " ");
+        }
+        Console.WriteLine("");
+    }
+}
---
+++ HWork_CSHarp_8/Example_60/Program.cs
@@ -0,0 +1,68 @@
+﻿/* Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)*/
+
+Console.Write("Введите количество строк массива: ");
+int rows=int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns=int.Parse(Console.ReadLine());
+
+Console.Write("Введите ширину массива: ");
+int number=int.Parse(Console.ReadLine()); 
+
+int[,,] array = GetArray(rows, columns,number); //вводим метод рандомного заполнения массива 
+//PrintArray(array); 
+PrintIndex(array);
+
+int[,,] GetArray(int rows, int columns, int number, int minValue=0, int maxValue=100)
+{
+    int[,,] result = new int[rows,columns,number];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            for (int k = 0; k < columns; k++)
+            {
+                 result[i, j, k] = new Random().Next(minValue, maxValue + 1);
+            }
+        }
+    }
+    return result;
+}
+/*
+void PrintArray (int[,,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+          for (int k = 0; k < array.GetLength(1); k++)
+          {
+            Console.Write(array[i,j,k] );
+          }
+    Console.WriteLine();
+    }
+  }
+}
+*/
+
+// Функция вывода индекса элементов 3D массива
+void PrintIndex(int[,,] arr)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
